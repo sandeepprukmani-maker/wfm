e71eeef7ecfa5d7bb07f54630957fdc516f7ea82
Sandeep <sandeep.p.rukmani@gmail.com>
2026-01-28 05:30:02 +0530

    5678

--- server_py/fetch_token.py
+++
@@ -1,7 +0,0 @@
-def fetch_token():
-    # Placeholder for the actual fetch_token logic as described by the user
-    # Returning the expected dictionary format
-    return {
-        "access_token": "your_access_token_here",
-        "baseUrl": "your_base_url_here"
-    }
